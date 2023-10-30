a87f6940611bd501add9169c9c749ef0fd374a8a
Hellodyonz <hellodyonz@gmail.com>
2023-10-30 20:45:03 +0700

    edit heading

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -1,6 +1,6 @@
 function Header(){
     return(
-      <h1 className="text-3xl font-bold text-blue-900 my-4">Apa yang harus dilakukan hari inii</h1>
+      <h1 className="text-3xl font-bold text-blue-900 my-4">Apa yang harus dilakukan hari ini</h1>
     )
   }
 
